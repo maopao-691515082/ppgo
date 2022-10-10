571143687be7c50ba13151f98201114998d96192
maopao <maopao@micous.com>
2022-10-10 17:10:43 +0800

    save

--- lib/time/time.cpp
+++ lib/time/time.cpp
@@ -8,9 +8,9 @@ namespace ppgo
 namespace PPGO_THIS_MOD
 {
 
-::ppgo::Exc::Ptr func_time(::std::tuple<::ppgo::tp_float> &ret)
+::ppgo::Exc::Ptr func_time_us(::std::tuple<::ppgo::tp_int> &ret)
 {
-    ::std::get<0>(ret) = ::ppgo::util::NowUS() / 1e6;
+    ::std::get<0>(ret) = ::ppgo::util::NowUS();
     return nullptr;
 }
 
